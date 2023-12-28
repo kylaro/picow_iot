5723f99f1ac9c72be4142ebeea8f9cc95461cbdb
Kylar <kylar.osborne@gmail.com>
2023-12-28 10:53:29 +0900

    update script.js

--- website/script.js
+++ website/script.js
@@ -21,13 +21,17 @@ function fetchDataAndRenderChart(timeRange) {
         })
         .catch(error => console.error('Error fetching data:', error));
     //hide spinner
-    const spinner = document.getElementById('spinner');
-    spinner.style.display = 'none';
+    // const spinner = document.getElementById('spinner');
+    // spinner.style.display = 'none';
 }
 
 function renderChart(data) {
     const ctx = document.getElementById('environmentChart').getContext('2d');
-    const timestamps = data.map(item => item.timestamp);//data.map(item => new Date(item.timestamp).toLocaleTimeString());
+    //const timestamps = data.map(item => item.timestamp);//data.map(item => new Date(item.timestamp).toLocaleTimeString());
+    //const timestamps = data.map(item => new Date(new Date(item.timestamp).getTime() + 60*60*1000)); // Has the timezone included
+    const timestamps = data.map(item => new Date(new Date(item.timestamp).getTime() + 60*60*1000).toLocaleString('en-US', { timeZoneName: 'short' }).split(' GMT')[0]);
+    //const timestamps = data.map(item => new Date(new Date(item.timestamp).getTime() + 60*60*1000).toJSON().slice(0, -5));
+
     const temperatures = data.map(item => item.temperature);
     const humidities = data.map(item => item.humidity);
     const co2Levels = data.map(item => item.co2);
@@ -44,8 +48,8 @@ function renderChart(data) {
                     borderColor: 'rgba(0, 123, 255, 1)',
                     backgroundColor: 'rgba(0, 123, 255, 0.5)',
                     yAxisID: 'yTemperature', // Corrected yAxisID
-                    pointHitRadius: 10,
-                    pointRadius: 2,      // Smaller value for smaller points
+                    pointHitRadius: 7,
+                    pointRadius: 1,      // Smaller value for smaller points
                     borderWidth: 1       // Smaller value for thinner line
                 },
                 {
@@ -54,8 +58,8 @@ function renderChart(data) {
                     borderColor: 'rgba(40, 167, 69, 1)',
                     backgroundColor: 'rgba(40, 167, 69, 0.5)',
                     yAxisID: 'yHumidity', // Corrected yAxisID
-                    pointHitRadius: 10,
-                    pointRadius: 2,      // Smaller value for smaller points
+                    pointHitRadius: 7,
+                    pointRadius: 1,      // Smaller value for smaller points
                     borderWidth: 1       // Smaller value for thinner line
                 },
                 {
@@ -64,8 +68,8 @@ function renderChart(data) {
                     borderColor: 'rgba(220, 53, 69, 1)',
                     backgroundColor: 'rgba(220, 53, 69, 0.5)',
                     yAxisID: 'yCo2', // Corrected yAxisID
-                    pointHitRadius: 10,
-                    pointRadius: 2,      // Smaller value for smaller points
+                    pointHitRadius: 7,
+                    pointRadius: 1,      // Smaller value for smaller points
                     borderWidth: 1       // Smaller value for thinner line
                 }
             ],
